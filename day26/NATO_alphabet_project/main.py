e70d7d58da338179c1338a55b4a9495fb1aacd8f
Nilanka Biswas <52482681+nilanka76@users.noreply.github.com>
2023-09-21 12:41:19 +0530

    added day26 and day27

---
+++ day26/NATO_alphabet_project/main.py
@@ -0,0 +1,22 @@
+import pandas
+
+data = pandas.read_csv("nato_phonetic_alphabet.csv")
+nato_phonetic_data_frame = pandas.DataFrame(data)
+
+# Coverting the pandas dataframe into a dictionary | Dictionary Comprehension
+nato_dict = {
+   row.letter: row.code for (_, row) in nato_phonetic_data_frame.iterrows()
+}
+
+# Converting the word into the NATO phonetic code list | List Comprehension
+def nato_converter():
+   word = input("Enter the word: ").upper()
+   try:
+      nato_code_list = [nato_dict[letter] for letter in word]
+   except KeyError:
+      print("Only letters are allowed")
+      nato_converter()
+   else:
+      print(nato_code_list)
+
+nato_converter()
